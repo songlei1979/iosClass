1c0285327a318ebe4acef3da577cda7f10c5e3c6
Lei <gabriel_sl19798@hotmail.com>
2019-09-10 16:53:15 +1200

    version 1

---
+++ week8/week8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week8
+//
+//  Created by Lei Song on 10/09/19.
+//  Copyright © 2019 Lei Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ week8/week8Tests/week8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  week8Tests.swift
+//  week8Tests
+//
+//  Created by Lei Song on 10/09/19.
+//  Copyright © 2019 Lei Song. All rights reserved.
+//
+
+import XCTest
+@testable import week8
+
+class week8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
